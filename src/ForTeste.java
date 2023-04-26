603956c080adab715c7cdc4ce7a173a076e09095
thiago <thiago.alves@ifce.edu.br>
2023-04-25 21:03:41 -0300

    Introducao ao for

--- src/ForTeste.java
+++ src/ForTeste.java
@@ -1,8 +1,16 @@
 public class ForTeste {
 
 	public static void main(String[] args){
-		for (int counter = 1; counter <= 10; counter++) {
-			System.out.printf("%d ", counter);
+//		for (int counter = 1; counter <= 10; counter++) {
+//			System.out.printf("%d ", counter);
+//		}
+
+//		for (int i = 100; i >= 1; i = i - 1){
+//			System.out.printf("%d ", i);
+//		}
+
+		for (int i = 7; i <= 77; i = i + 7){//i += 7
+			System.out.printf("%d ", i);
 		}
 	}
 }
